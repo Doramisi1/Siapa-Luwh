b12453a482e7b356c6d02df07c46c6c2aa6cb394
Bl4ck-lion <67418383+Bl4ck-lion@users.noreply.github.com>
2022-05-13 14:02:56 +0700

    Add files via upload

--- plugins/gfx-gfx1.js
+++ plugins/gfx-gfx1.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch'
   import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command, args }) => {
   
-  let res = await(await fetch(`https://api-xzn-yotsuya.up.railway.app/docs/gfx1?name=${args[0]}`)
+  let res = await(await fetch(`https://api-xzn-yotsuya.up.railway.app/docs/gfx1?name=${args[0]}`)).buffer()
   if(!args[0]) throw `Linknya mana?`
 conn.sendHydrated(m.chat, `*「 GFX MAKER 」*\n\n*💻 Username:* ${args[0]}`, null, res, `youtube.com/c/LeonGanz`, '🌎 U R L', null, null, [[null,null],[null,null],[null,null]], m)
 }
 handler.help = ['gfx1 <namakalian>]
 handler.tags = ['gfx']
-handler.command = /^gfx1$/i
+handler.command = /^(gfx1)$/i
 export default handler
--- plugins/internet-ssweb.js
+++ plugins/internet-ssweb.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch'
   import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command, args }) => {
   
-  let res = await(await fetch(`https://api.lolhuman.xyz/api/ssweb?apikey=e54205a4ca2caa368cc067bb&url=${args[0]}`)
+  let res = await(await fetch(`https://api.lolhuman.xyz/api/ssweb?url=${args[0]}&apikey=e54205a4ca2caa368cc067bb`)).buffer()
   if(!args[0]) throw `Linknya mana?`
 conn.sendHydrated(m.chat, `*「 Screenshot Web 」*\n\n*💻 Url:* ${args[0]}`, null, res, `${args[0]}`, '🌎 U R L', null, null, [[null,null],[null,null],[null,null]], m)
 }
 handler.help = ['ssweb']
 handler.tags = ['internet']
-handler.command = /^ss(web)?|scre?e?nshu?o?t$/i
+handler.command = /^(ssweb)$/i
 export default handler
