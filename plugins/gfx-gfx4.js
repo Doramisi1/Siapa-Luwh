fd3877dc00eacc6d79665da6958b88c24b2c100d
Bl4ck-lion <67418383+Bl4ck-lion@users.noreply.github.com>
2022-05-13 15:20:04 +0700

    Add files via upload

--- plugins/gfx-gfx4.js
+++ plugins/gfx-gfx4.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
   import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command, args }) => {
   
-  let res = await(await fetch(`https://api-xzn-yotsuya.up.railway.app/docs/gfx4?name=${args[0]}`)).buffer()
+  let res = await(await fetch(`https://api-xzn-yotsuya.up.railway.app/docs/gfx4?name=${args[0]}&name2=${args[1]}`)).buffer()
   if(!args[0]) throw `Nama Username?`
 conn.sendButton(m.chat, 'Nih Gfx nya >_<', author, res, [['done', `${usedPrefix}waifu`]], m)
 }
