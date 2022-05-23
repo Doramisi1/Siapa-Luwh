e2ae26d49ba70427db1cfa71a977ce131b5d0db0
Bl4ck-lion <67418383+Bl4ck-lion@users.noreply.github.com>
2022-05-23 12:46:23 +0700

    Update anime-husbu.js

--- plugins/anime-husbu.js
+++ plugins/anime-husbu.js
@@ -1,14 +1,21 @@
 import fetch from 'node-fetch'
+import fs from 'fs'
 
 let handler = async (m, { conn, usedPrefix }) => {
     let res = await fetch('https://recoders-area.caliph.repl.co/api/husbu?apikey=FreeApi')
     if (!res.ok) throw await res.text()
     let json = await res.json()
     if (!json.url) throw 'Error!'
-    conn.sendFile(m.chat, json.url, 'husbu.jpg', Suami Kartun, m, false)
+    //conn.sendButton(m.chat, 'Suami kok kartun', botdate + '\n\n' + wm, json.url, [['Husbu', `${usedPrefix}husbu`]], m)
+    conn.sendButton(m.chat, 'Suami kok kartun', botdate + '\n\n' + wm, json.url, [['Husbu', `${usedPrefix}husbu`]], m, {
+contextInfo: { externalAdReply :{
+                        description: stickauth,
+                        title: author,
+                        body: 'Jangan lupa Follow',
+                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
+                        sourceUrl: sig }}})
 }
 handler.help = ['husbu']
 handler.tags = ['anime']
 handler.command = /^(husbu)$/i
-//MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
 export default handler
